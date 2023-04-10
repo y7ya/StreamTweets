ca44b2b64880e15e1b4dabb1fa46efa85737e504
Yahya <yhya9813@gmail.com>
2023-04-10 20:48:41 +0300

    change background color of tweet to red

--- StreamTweets.js
+++ StreamTweets.js
@@ -5,7 +5,11 @@ const observer = new MutationObserver(function (mutations, observer) {
   for (const mutation of mutations) {
     if (mutation.type !== "childList") continue;
     for (const node of mutation.addedNodes) {
-      if(node.attributes[0]?.nodeName !== "data-testid") continue;
+      if(hasLive(node)){
+        if(node.closest('[data-testid="tweet"]')){
+          node.closest('[data-testid="tweet"]').style.backgroundColor = 'red';
+        }
+      }
     }
   }
 }).observe(target, {
